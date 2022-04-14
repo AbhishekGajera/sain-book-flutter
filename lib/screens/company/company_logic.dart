e44cc79d7a0ecdf4e6b52b30535abc815362dada
parthkoladiya108 <parthkoladiya108@gmail.com>
2022-04-14 22:36:05 +0530

    parth'fs irst commit

--- lib/screens/company/company_logic.dart
+++ lib/screens/company/company_logic.dart
@@ -3,5 +3,5 @@ import 'package:get/get.dart';
 import 'company_state.dart';
 
 class CompanyLogic extends GetxController {
-  final CompanyState state = CompanyState();
+  final CompanyState state = CompanyState(); 
 }
